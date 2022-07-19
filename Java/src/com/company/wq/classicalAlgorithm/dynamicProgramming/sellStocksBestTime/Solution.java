bca7c670806315a10f62c2fb4618ea480f938943
theSunshine666 <34849039+theSunshine666@users.noreply.github.com>
2022-07-19 15:31:37 +0800

    Update Solution.java

--- Java/src/com/company/wq/classicalAlgorithm/dynamicProgramming/sellStocksBestTime/Solution.java
+++ Java/src/com/company/wq/classicalAlgorithm/dynamicProgramming/sellStocksBestTime/Solution.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+/**
+ * 买卖股票的最佳时间（一）
+ * 从头到尾只能买一次卖一次
+ */
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,13 +19,19 @@ public class Solution {
             for (int i = 0; i < n; i++) {
                 prices[i] = Integer.parseInt(priceStr[i]);
             }
-//            System.out.println(Arrays.toString(prices));
             System.out.println(maxProfit(prices));
 
         }
         br.close();
     }
 
+    /**
+     * 解法一：动态规划算法：
+     * 运行时间：16ms 占用内存：9900KB
+     * 两层遍历：dp[i] = Math.max(curr,prices[i]-prices[j]);
+     * @param prices
+     * @return
+     */
     public static int maxProfit(int[] prices){
         int n = prices.length;
         if (n <= 1){
@@ -42,5 +51,26 @@ public class Solution {
             }
         }
         return dp[n-1];
+    }
+
+    /**
+     * 解法二：贪心算法：
+     * 运行时间：21ms 占用内存：9760KB
+     * 一遍遍历，贪心算法认为这组数组中总有一个最小价格，只要遇到一个比它价格高的且比当前最优解
+     * （当前最大利润）更大的就卖掉，遇到比最小价格还小的就更新最小价格
+     * @param prices
+     * @return
+     */
+    public static int maxProfit2(int[] prices){
+        int minPrice = Integer.MAX_VALUE;
+        int res = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice){
+                minPrice = prices[i];
+            }else if (prices[i] - minPrice > res){
+                res = prices[i] - minPrice;
+            }
+        }
+        return res;
     }
 }
